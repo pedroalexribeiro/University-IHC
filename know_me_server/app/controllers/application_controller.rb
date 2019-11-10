fcf06ec4f6f8ca6fc3bc5fede1b7b7e25ea74e01
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-11-10 01:26:31 +0000

    add need include to knock gem

--- know_me_server/app/controllers/application_controller.rb
+++ know_me_server/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# application_controller.rb
 class ApplicationController < ActionController::Base
+  include Knock::Authenticable
+  # before_action :authenticate_user
 end
