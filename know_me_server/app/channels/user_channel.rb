e1dffebce8c6812d8926ffafb459485a16f9c284
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-11-13 02:16:41 +0000

    web socket method to create room

--- know_me_server/app/channels/user_channel.rb
+++ know_me_server/app/channels/user_channel.rb
@@ -6,6 +6,13 @@ class UserChannel < ApplicationCable::Channel
     stream_from "user_#{uuid}"
   end
 
-  def check_game_room
+  def check_game_room(args)
+    code = args['code']
+    game_room = GameRoom.find(code: code)
+    if game_room
+      ActionCable.server.broadcast "user_#{uuid}", notification: 'Welcome'
+    else
+      ActionCable.server.broadcast "user_#{uuid}", notification: 'Error, couldn\'t find'
+    end
   end
 end
