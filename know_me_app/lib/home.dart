7db38bcfdaa6125d3f718f8b9270593afcbc2678
KinryuBarao <s.franciscobarao@gmail.com>
2019-11-13 02:32:45 +0000

    edited home to take start out and now its a tap on screen

--- know_me_app/lib/home.dart
+++ know_me_app/lib/home.dart
@@ -8,30 +8,31 @@ class Home extends StatelessWidget {
   final String title;
 
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          stops: [0.3, 0.7],
-          colors: [
-            Color.fromRGBO(107, 185, 108, 1),
-            Color.fromRGBO(11, 153, 142, 1),
-          ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute<Null>(
+            builder: (BuildContext context) => RoomSelect()));
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            stops: [0.3, 0.7],
+            colors: [
+              Color.fromRGBO(107, 185, 108, 1),
+              Color.fromRGBO(11, 153, 142, 1),
+            ],
+          ),
         ),
-      ),
-      child: Scaffold(
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              KnowMeImage('assets/images/logo.png', 130, 160),
-              Container(
-                child: Center(
-                  child: Button('Start Game'),
-                ),
-              ),
-            ]),
+        child: Scaffold(
+          body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                KnowMeImage('assets/images/logo.png', 130, 160),
+              ]),
+        ),
       ),
     );
   }
@@ -53,32 +54,5 @@ class KnowMeImage extends StatelessWidget {
         height: _height,
       ),
     );
-  }
-}
-
-class Button extends StatelessWidget {
-  final String _text;
-  Button(this._text);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialButton(
-        child: Text(
-          _text,
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-        ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        height: 40,
-        minWidth: 220,
-        elevation: 2,
-        color: Colors.white,
-        textColor: Color.fromRGBO(97, 97, 96, 1),
-        onPressed: () {
-          //Use`Navigator` widget to push the second screen to out stack of screens
-          Navigator.of(context)
-              .push(MaterialPageRoute<Null>(builder: (BuildContext context) {
-            return new RoomSelect();
-          }));
-        });
   }
 }
