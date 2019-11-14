9764b5f4811d1b946b3fb98bf7d1d942b146f16f
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2019-11-14 17:02:34 +0000

    its working i think

--- know_me_server/app/channels/game_channel.rb
+++ know_me_server/app/channels/game_channel.rb
@@ -5,4 +5,9 @@ class GameChannel < ApplicationCable::Channel
   def subscribed
     stream_from "game_channel_#{request.params[:room]}"
   end
+
+  def information(args)
+    game_room = GameRoom.find(code: args['code'])
+    game_room.users.create(name: args['name'], icon: args['icon'])
+  end
 end
--- know_me_server/app/controllers/landing_controller.rb
+++ know_me_server/app/controllers/landing_controller.rb
@@ -2,20 +2,49 @@
 
 # Landing Controller
 class LandingController < ApplicationController
+  before_action :set_game_room, except: %i[index create_room fill]
+
   def index; end
 
   def create_room
-    code = (0...5).map { (65 + rand(26)).chr }.join
-    game_room = GameRoom.new(code: code, active: true)
+    @code = (0...5).map { (65 + rand(26)).chr }.join
+    game_room = GameRoom.new(code: @code, active: true)
     if game_room.save
-      redirect_to firms_path
+      session[:game_room] = @code
+      render :create_room
     else
       flash[:error] = 'Some error while creating game room'
       render :index
     end
   end
 
-  def send_possible_answers
-    ActionCable.server.broadcast "game_channel_#{request.params[:room]}", notification: 'Error, couldn\'t find'
+  def game_room
+    @number = params['number']
+    if @number < 5
+      @question_theme = QuestionTheme.find(name: 'default')
+      @question = @question_theme.questions.limit(@number - 1)
+      ActionCable.server.broadcast("game_channel_#{@game_room.code}", @game_room.send_answer)
+      respond_to do |format|
+        format.js { render :game_room }
+      end
+    else
+      redirect_to end_screen_path
+    end
+  end
+
+  def end_screen; end
+
+  def fill
+    question_theme = QuestionTheme.create(name: 'default')
+    Question.create(question_theme: question_theme, question_type: :general, name: 'Who\'s most likely to laugh at a teachers lame joke?')
+    Question.create(question_theme: question_theme, question_type: :general, name: 'Who would throw a rubber, hit the teacher and blame another person?')
+    Question.create(question_theme: question_theme, question_type: :general, name: 'Who\' most likely to be the teacher\'s pet?')
+    Question.create(question_theme: question_theme, question_type: :general, name: 'Who has cheated during the exame the most?')
+  end
+
+  private
+
+  def set_game_room
+    @game_room = GameRoom.find(code: session['game_room'])
   end
 end
